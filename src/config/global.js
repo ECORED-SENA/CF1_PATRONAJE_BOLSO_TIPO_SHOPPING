c1a5ca17b35b7b06d7d4c6524870988cb8517854
manuelechavarria <maechavarriaor@gmail.com>
2025-08-04 09:12:09 -0500

    Ajustes

--- src/config/global.js
+++ src/config/global.js
@@ -173,7 +173,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/xxxxx_CF01_DU.pdf',
       },
       {
         icono: 'fas fa-download',
